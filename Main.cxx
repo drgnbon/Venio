2870e9e3572e29d0c1a992e99966bd751d3bdb9d
DrgNBoN <evgendova@gmail.com>
2024-06-13 19:59:34 +0300

    save3

--- Main.cxx
+++ Main.cxx
@@ -6,5 +6,5 @@ int main()
     Eigen::MatrixXd wqeqwe = Eigen::MatrixXd::Random(4,3);
     Eigen::MatrixXd wqeqwe2 = Eigen::MatrixXd::Random(3,2);
     Eigen::MatrixXd  cock = wqeqwe*wqeqwe2;
-    std::cout << cock;
+    std::cout << cock << "";
 }
