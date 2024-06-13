d8ff4551b0994eb54401de8c129d6ac388138879
DrgNBoN <evgendova@gmail.com>
2024-06-13 19:53:30 +0300

    save

--- Main.cxx
+++ Main.cxx
@@ -1,29 +1,10 @@
+#include <Eigen/Core>
+#include <iostream>
 
-#include <Venio/Venio.hxx>
-
-int main() {
-
-  Eigen::setNbThreads(6);
-  
-  NeuralNetwork network;
-  network.setLayers<PerceptronLayer>({10000,5,2});
-  network.setActivationFunction<Sinc>();
-
-  TrainerStrategy trainer(network);
-  trainer.setLossFunction<SquareError>();
-  trainer.setOptimizer<ADAM>(network);
-  //trainer.setHyperparameters(0.999,0.9,1e-8);
-
-  Logging log;
-  log.startLogging(trainer);
-
-  Branch branch(10000,2);
-  branch.generateRandomBranch(10,0,0.9);
-  branch.buildBranch();
-
-  trainer.fit(branch, 10000,0.002,false);
-
-  log.stopLogging();
+int main()
+{
+    Eigen::MatrixXd wqeqwe = Eigen::MatrixXd::Random(4,3);
+    Eigen::MatrixXd wqeqwe2 = Eigen::MatrixXd::Random(3,2);
+    Eigen::MatrixXd  cock = wqeqwe*wqeqwe2;
+    std::cout << cock;
 }
-
-
