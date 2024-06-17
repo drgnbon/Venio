e8e8b1b878086da0d366de6c07ea076645cb6951
DrgNBoN <evgendova@gmail.com>
2024-06-17 17:47:53 +0300

    save

---
+++ build/eigen-3.4.0/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/Venio/eigen-3.4.0
+# Build directory: D:/Projects/Venio/build/eigen-3.4.0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("test")
+subdirs("failtest")
+subdirs("blas")
+subdirs("lapack")
+subdirs("unsupported")
+subdirs("demos")
+subdirs("scripts")
---
+++ build/eigen-3.4.0/lapack/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/Venio/eigen-3.4.0/lapack
+# Build directory: D:/Projects/Venio/build/eigen-3.4.0/lapack
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
