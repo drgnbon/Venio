d682235eeeee9171275b3ad52215f4c140edeb6f
pixaut <116499977+pixaut@users.noreply.github.com>
2024-06-18 23:22:12 +0300

    updating start

--- Main.cxx
+++ Main.cxx
@@ -22,7 +22,7 @@ public:
         return rng_coin(gen);
     }
 
-    static Matrixd generateRandomMatrix(double min_rnd, double max_rnd, size_t cols, size_t rows) {
+    static Matrixd generateRandomMatrix(double min_rnd, double max_rnd, size_t rows, size_t cols) {
         std::random_device random_device;
         std::mt19937 gen(random_device());
         std::uniform_real_distribution<double> rng_coin(min_rnd, max_rnd);
@@ -110,12 +110,13 @@ class LogisticFunction : public ActivationFunction
 public:
     double toActivateValue(double x) override
     {
-        return double(1.f / (1.f + double(exp(-double(x)))));
+        return 1.f / (1.f + exp(-x));
     }
 
     double toDerivateValue(double x) override
     {
-        return double(double(toActivateValue(x)) * double(1.f - double(toActivateValue(x))));
+        double activatedValue = toActivateValue(x);
+        return activatedValue * (1.f - activatedValue);
     }
 };
 class LinearFunction : public ActivationFunction
@@ -136,12 +137,12 @@ class SoftSignFunction : public ActivationFunction
 public:
     double toActivateValue(double x) override
     {
-        return x / (1 + abs(x));
+        return x / (1.0 + fabs(x));  // Используйте std::abs
     }
 
     double toDerivateValue(double x) override
     {
-        return 1 / pow(1 + abs(x), 2);
+        return 1.0 / std::pow(1.0 + fabs(x), 2);  // Используйте std::pow и std::abs
     }
 };
 class SincFunction : public ActivationFunction
@@ -149,19 +150,16 @@ class SincFunction : public ActivationFunction
 public:
     double toActivateValue(double x) override
     {
-        if (x == 0)
-            return 1;
+        if (fabs(x) < 1e-8) return 1.0;
         return sin(x) / x;
     }
 
     double toDerivateValue(double x) override
     {
-        if (x == 0)
-            return 0;
-        return (cos(x) / x) - (sin(x) / pow(x, 2));
+        if (fabs(x) < 1e-8) return 0.0;
+        return (cos(x) / x) - (sin(x) / (x * x));
     }
 };
-
 class GhFunction : public ActivationFunction
 {
 public:
@@ -181,26 +179,21 @@ public:
 class LossFunction
 {
 public:
-    virtual double getMediumLoss(Matrixd active_value, Matrixd right_answer) = 0;
-
-    virtual Matrixd getDerivationLoss(Matrixd active_value, Matrixd right_answer) = 0;
+    virtual double getMediumLoss(const Matrixd& activeValue, const Matrixd& rightAnswer) = 0;
+    virtual Matrixd getDerivationLoss(const Matrixd& activeValue, const Matrixd& rightAnswer) = 0;
 };
 class SquareErrorFunction : public LossFunction
 {
 public:
-    double getMediumLoss(Matrixd active_value, Matrixd right_answer) override
+    double getMediumLoss(const Matrixd& activeValue, const Matrixd& rightAnswer) override
     {
-        double SquareError = 0;
-        for (int i = 0; i < active_value.rows(); ++i)
-            for (int j = 0; j < active_value.cols(); ++j)
-                SquareError += (active_value(i, j) - right_answer(i, j)) * (active_value(i, j) - right_answer(i, j));
-        return SquareError;
+        double squareError = (activeValue - rightAnswer).squaredNorm();
+        return squareError / static_cast<double>(activeValue.size());
     }
 
-    // Probably warning---------------------------
-    Matrixd getDerivationLoss(Matrixd active_value, Matrixd right_answer) override
+    Matrixd getDerivationLoss(const Matrixd& activeValue, const Matrixd& rightAnswer) override
     {
-        return 2 * (active_value - right_answer);
+        return 2.0 * (activeValue - rightAnswer);
     }
 };
 // LossFunctions---------------------------------------------------------------------------------------------------------
@@ -218,43 +211,66 @@ protected:
     int _layer_size;
 
 public:
+
     Layer(int layer_size, ActivationFunction *activation_function)
     {
         _layer_size = layer_size;
         _activation_function = activation_function;
     }
-
     virtual ~Layer() {}
-
     void buildLayer(int output_size_of_last_layer)
     {
-        _weights = Matrixd::Random(output_size_of_last_layer, _layer_size);
+        // Генерация случайных весов для связей между слоями
+        _weights = RandomGenerator::generateRandomMatrix(0.001, 0.999, output_size_of_last_layer, _layer_size);
+
+        // Инициализация градиентов весов нулями
         _weights_gradient = Matrixd::Zero(output_size_of_last_layer, _layer_size);
+
+        // Инициализация активных значений слоя нулями
         _active_values = Matrixd::Zero(1, _layer_size);
+
+        // Инициализация значений слоя нулями
         _values = Matrixd::Zero(1, _layer_size);
-        _bias = Matrixd::Random(1, _layer_size);
+
+        // Генерация случайных значений для смещений (bias)
+        _bias = RandomGenerator::generateRandomMatrix(0.001, 0.999, 1, _layer_size);
+
+        // Инициализация градиентов смещений нулями
         _bias_gradient = Matrixd::Zero(1, _layer_size);
+
+        // Инициализация производных нейронов нулями
         _derivation_neurons = Matrixd::Zero(1, _layer_size);
     }
-
     void buildFirstLayer(){
+        // Инициализация активных значений слоя нулями
         _active_values = Matrixd::Zero(1, _layer_size);
+
+        // Инициализация значений слоя нулями
+        _values = Matrixd::Zero(1, _layer_size);
+
+        // Генерация случайных значений для смещений (bias)
+        _bias = RandomGenerator::generateRandomMatrix(0.001, 0.999, 1, _layer_size);
+
+        // Инициализация градиентов смещений нулями
+        _bias_gradient = Matrixd::Zero(1, _layer_size);
+
+        // Инициализация производных нейронов нулями
+        _derivation_neurons = Matrixd::Zero(1, _layer_size);
     }
 
     void activateLayer()
     {
         _active_values = _activation_function->toActivateMatrix(_values);
     }
-
     virtual void propogateLayer(Matrixd last_layer_output) = 0;
-
     virtual void backPropogateLayer(Matrixd next_layer_derivation, Matrixd next_layer_values, Matrixd next_layer_weights, Matrixd last_active_values) = 0;
 
     // getters & setters for class Layer---------------------------------------------------------------------
 
     void setLayerDerivation(Matrixd new_derivation_neurons_matrix)
     {
-        if (!(new_derivation_neurons_matrix.cols() == _derivation_neurons.cols() && new_derivation_neurons_matrix.rows() == _derivation_neurons.rows()))
+        if (!(new_derivation_neurons_matrix.cols() == _derivation_neurons.cols()
+           && new_derivation_neurons_matrix.rows() == _derivation_neurons.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerDerivation (Not equal size of matrix) - class Layer \n");
         }
@@ -263,7 +279,8 @@ public:
     }
     void setLayerBias(Matrixd new_bias_matrix)
     {
-        if (!(new_bias_matrix.cols() == _bias.cols() && new_bias_matrix.rows() == _bias.rows()))
+        if (!(new_bias_matrix.cols() == _bias.cols()
+           && new_bias_matrix.rows() == _bias.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerBias (Not equal size of matrix) - class Layer \n");
         }
@@ -272,16 +289,17 @@ public:
     }
     void setLayerBiasGradient(Matrixd new_bias_gradient_matrix)
     {
-        if (!(new_bias_gradient_matrix.cols() == _bias_gradient.cols() && new_bias_gradient_matrix.rows() == _bias_gradient.rows()))
+        if (!(new_bias_gradient_matrix.cols() == _bias_gradient.cols()
+           && new_bias_gradient_matrix.rows() == _bias_gradient.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerBiasGradient (Not equal size of matrix) - class Layer \n");
         }
         _bias_gradient = std::move(new_bias_gradient_matrix);
     }
-
     void setLayerValues(Matrixd new_values_matrix)
     {
-        if (!(new_values_matrix.cols() == _values.cols() && new_values_matrix.rows() == _values.rows()))
+        if (!(new_values_matrix.cols() == _values.cols()
+           && new_values_matrix.rows() == _values.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerValues (Not equal size of matrix) - class Layer \n");
         }
@@ -290,7 +308,8 @@ public:
     }
     void setLayerActiveValues(Matrixd new_active_values_matrix)
     {
-        if (!(new_active_values_matrix.cols() == _active_values.cols() && new_active_values_matrix.rows() == _active_values.rows()))
+        if (!(new_active_values_matrix.cols() == _active_values.cols()
+           && new_active_values_matrix.rows() == _active_values.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerActiveValues (Not equal size of matrix) - class Layer \n");
         }
@@ -299,28 +318,36 @@ public:
     }
     void setLayerWeights(Matrixd new_weights_matrix)
     {
-        if (!(new_weights_matrix.cols() == _weights.cols() && new_weights_matrix.rows() == _weights.rows()))
+        if (!(new_weights_matrix.cols() == _weights.cols()
+           && new_weights_matrix.rows() == _weights.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerWeights (Not equal size of matrix) - class Layer \n");
         }
 
         _weights = std::move(new_weights_matrix);
     }
-
     void setLayerWeightsGradient(Matrixd new_weights_gradient_matrix)
     {
 
-        if (!(new_weights_gradient_matrix.cols() == _weights_gradient.cols() && new_weights_gradient_matrix.rows() == _weights_gradient.rows()))
+        if (!(new_weights_gradient_matrix.cols() == _weights_gradient.cols()
+           && new_weights_gradient_matrix.rows() == _weights_gradient.rows()))
         {
             ErrorLogger::getInstance().logError("Error in setLayerWeightsGradient (Not equal size of matrix) - class Layer \n");
         }
 
         _weights_gradient = std::move(new_weights_gradient_matrix);
     }
-
+    // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
     void setLayerActivationFunction(ActivationFunction *new_activation_function)
     {
-        _activation_function = new_activation_function; // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+        // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+        try{
+            _activation_function = new_activation_function;
+        }catch(std::exception& e){
+            std::cerr << "Activation function error: " << e.what() << "\n";
+        }
+        // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+
     }
 
     // getters--------------------------------------------------------
@@ -349,15 +376,19 @@ public:
     {
         return _weights_gradient;
     }
+    //Rename or function name or name of value
     Matrixd getLayerDerivation()
     {
         return _derivation_neurons;
     }
-    ActivationFunction *getLayerActivationFunction()
+    // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+    ActivationFunction* getLayerActivationFunction()
     {
-        return _activation_function; // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+        // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
+        return _activation_function;
+        // WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING - WARNING
     }
-    size_t getLayerSize()
+    size_t getLayerSize() const
     {
         return _layer_size;
     }
@@ -376,21 +407,14 @@ public:
         _values = last_layer_output * _weights + _bias;
         activateLayer();
     }
-
     void backPropogateLayer(Matrixd next_layer_derivation, Matrixd next_layer_values, Matrixd next_layer_weights, Matrixd last_active_values) override
     {
-
-        Matrixd next_derivation_neurons /*(with af)*/ = Matrixd(next_layer_derivation.array() * _activation_function->toDerivateMatrix(next_layer_values).array());
-
-        _derivation_neurons = next_derivation_neurons * next_layer_weights.transpose();
-
-        _weights_gradient = last_active_values.transpose() * _derivation_neurons;
-
-        _bias_gradient = _derivation_neurons;
+        //Do work---------------------------------------------------------------------
     }
 };
 
 // work in progress--------------
+/*
 class ConvolutionLayer : Layer
 {
 public:
@@ -405,8 +429,10 @@ public:
     {
         // Work in progress--------------------------
     }
-};
+};*/
+
 // Layers----------------------------------------------------------------------------------------------------------------
+
 
 class Model
 {
@@ -474,23 +500,11 @@ public:
         // Add activation to output (maybe softmax)-----------------
         _output = _layers[_layers.size() - 1]->getLayerActiveValues();
     }
-
     void backPropogation(Matrixd right_answer)
     {
-        // if do better output change this code {
-        _layers[_layers.size() - 1]->setLayerDerivation(_loss_function->getDerivationLoss(_output, right_answer).array() * _layers[_layers.size() - 1]->getLayerActivationFunction()->toDerivateMatrix(_layers[_layers.size() - 1]->getLayerValues()).array());
-        // }
 
-        _layers[_layers.size() - 1]->setLayerWeightsGradient(_layers[_layers.size() - 2]->getLayerActiveValues().transpose() * _layers[_layers.size() - 1]->getLayerDerivation());
-        _layers[_layers.size() - 1]->setLayerBiasGradient(_layers[_layers.size() - 1]->getLayerDerivation());
+        //Do work------------------------------------------------------------------
 
-        for (int i = _layers.size() - 2; i >= 1; --i)
-        {
-            _layers[i]->backPropogateLayer(_layers[i + 1]->getLayerDerivation(),
-                                           _layers[i + 1]->getLayerValues(),
-                                           _layers[i + 1]->getLayerWeights(),
-                                           _layers[i - 1]->getLayerActiveValues());
-        }
     }
 
     // getters & setters for class Model---------------------------------------------------------------------
@@ -650,9 +664,6 @@ public:
 
 int main()
 {
-    std::cout << RandomGenerator::generateRandomMatrix(0.5,0.6,2,5) << "\n";
-    system("pause");
-
 
     //srand(time(NULL));
 
@@ -689,19 +700,11 @@ int main()
         network.setInput(a);
         network.forwardPropogation();
         network.backPropogation(b);
-        gd.updateWeights(0.5, epoch);
+        gd.updateWeights(0.2, epoch);
 
         std::cout << network.getOutput() << "\n";
         //system("pause");
         ++epoch;
     }
 
-    //    Matrixd _bias,_bias_gradient;
-    //    Matrixd _values;
-    //    Matrixd _active_values;
-    //    Matrixd _derivation_neurons;
-    //    Matrixd _weights,_weights_gradient;
-
-    //    network.forwardPropogation();
-    //    network.backPropogation(b);
 }
