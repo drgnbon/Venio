d6e62f56cda3dea4ed030dfecdc591e0976df61e
vladd12 <vladd12z32@gmail.com>
2024-06-22 18:39:26 +0300

    Using CUDA as internal private dependency for CUDAlib

--- CUDAlib/GPUcalculation.h
+++ CUDAlib/GPUcalculation.h
@@ -1,25 +1,19 @@
 #ifndef GPUCALCULATION_H
 #define GPUCALCULATION_H
 
-#include <cublas_v2.h>
-#include <cuda_runtime.h>
 #include <Eigen/Dense>
-#include <iostream>
 
-class GPUcalculation
-{
+class GPUcalculation {
 public:
-    GPUcalculation();
-    ~GPUcalculation();
+  GPUcalculation();
+  ~GPUcalculation();
 
-    void getMatrixMultiply(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B);
-    Eigen::MatrixXd getMatrixTranspose(const Eigen::MatrixXd &A);
+  void getMatrixMultiply(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B);
+  Eigen::MatrixXd getMatrixTranspose(const Eigen::MatrixXd &A);
 
 private:
-    cublasHandle_t handle;
-
-    void checkCuda(cudaError_t result, const char *msg);
-    void checkCublas(cublasStatus_t result, const char *msg);
+  typedef void *internal_handle;
+  internal_handle handle;
 };
 
 #endif // GPUCALCULATION_H
